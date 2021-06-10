87e87a75fda245291d18542cb096be949306659a
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2021-06-09 23:57:42 -0300

    feat: config nodemailer

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,18 @@
+import Mail from '../lib/Mail';
+
 export default {
   async store(req, res) {
     const { name, email, password } = req.body;
 
     const user = { name, email, password };
 
-    // TODO send email
+    await Mail.sendMail({
+      from: 'Queue Test <queue@email.com>',
+      to: `${name} <${email}>`,
+      subject: 'Cadastro de usuário',
+      html: `Olá, ${name}, bem-vindo ao sistema de filas xD`,
+    });
+
     return res.json(user);
   },
 };
