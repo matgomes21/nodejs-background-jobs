c410f02450c976312e48c7e5778db822ec5561b4
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2021-07-07 10:38:22 -0300

    feat: add authorization middleware to bull board

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import { createBullBoard } from '@bull-board/api';
 import { BullAdapter } from '@bull-board/api/bullAdapter';
 import { ExpressAdapter } from '@bull-board/express';
 import express from 'express';
+import basicAuth from 'express-basic-auth';
 
 import UserController from './app/controllers/UserController';
 import Queue from './app/lib/Queue';
@@ -27,7 +28,14 @@ app.use(express.json());
 app.post('/users', UserController.store);
 
 serverAdapter.setBasePath('/admin/queues');
-app.use('/admin/queues', serverAdapter.getRouter());
+app.use(
+  '/admin/queues',
+  basicAuth({
+    users: { admin: process.env.BULL_PASSWORD },
+    challenge: true,
+  }),
+  serverAdapter.getRouter()
+);
 
 app.listen(3333, () => {
   console.log('Server running on localhost:3333');
